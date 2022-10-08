2837b82d9d9f020c27e91477c2bc5c9f6483d8e6
Flávia Correia <fllaviacorreia@outlook.com>
2022-10-07 21:14:26 -0300

    creating user migration and model

---
+++ database/migrations/1665187031717_users.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('full_name', 255).notNullable()
+      table.string('phone_number').notNullable()
+      table.string('email', 255).notNullable()
+      table.string('password', 255).notNullable()
+      table.string('slug', 20).notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
